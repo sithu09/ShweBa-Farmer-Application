01739e1c22406b3a6b60ea04d614bea4d16f9d4b
Sithu Lwin <lwinsithu3@gmail.com>
2019-10-06 13:24:12 +0630

    Login 0.1.0

--- app/src/main/java/com/mt/shweba/MainActivity.java
+++ app/src/main/java/com/mt/shweba/MainActivity.java
@@ -3,9 +3,13 @@ package com.mt.shweba;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
@@ -16,12 +20,15 @@ import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 Button btn;
+EditText uname,upassword;
 DatabaseReference reff;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
       btn=(Button)findViewById(R.id.button5);
+      uname=(EditText) findViewById(R.id.editText);
+      upassword=(EditText)findViewById(R.id.editText2);
         reff= FirebaseDatabase.getInstance().getReference().child("Member").child("1");
 
         btn.setOnClickListener(new View.OnClickListener() {
@@ -30,8 +37,18 @@ DatabaseReference reff;
 reff.addValueEventListener(new ValueEventListener() {
     @Override
     public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-        String name=dataSnapshot.child("name").getValue().toString();
-        Toast.makeText(MainActivity.this,name,Toast.LENGTH_LONG).show();
+        String name = dataSnapshot.child("name").getValue().toString();
+        String age = dataSnapshot.child("age").getValue().toString();
+        String user_name = uname.getText().toString().trim();
+        String user_password = upassword.getText().toString().trim();
+        if (uname.getText().toString().equals(name) && upassword.getText().toString().equals(age)) {
+            Intent intent = new Intent(MainActivity.this, Post.class);
+            startActivity(intent);
+        } else if (uname.length() == 0 || upassword.length() == 0) {
+            Toast.makeText(MainActivity.this, "Fill your name and password", Toast.LENGTH_LONG).show();
+        } else if(name != user_name || age != user_password) {
+            Toast.makeText(MainActivity.this, "User name of password wrong", Toast.LENGTH_LONG).show();
+        }
     }
 
     @Override
