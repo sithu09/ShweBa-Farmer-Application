85ad6f07b315a1384d20d2f563f49d95820513e0
Sithu Lwin <lwinsithu3@gmail.com>
2019-10-06 13:41:09 +0630

    Login 2/3

--- app/src/main/java/com/mt/shweba/MainActivity.java
+++ app/src/main/java/com/mt/shweba/MainActivity.java
@@ -3,9 +3,9 @@ package com.mt.shweba;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Context;
 import android.content.Intent;
 import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -31,32 +31,40 @@ DatabaseReference reff;
       upassword=(EditText)findViewById(R.id.editText2);
         reff= FirebaseDatabase.getInstance().getReference().child("Member").child("1");
 
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-reff.addValueEventListener(new ValueEventListener() {
-    @Override
-    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-        String name = dataSnapshot.child("name").getValue().toString();
-        String age = dataSnapshot.child("age").getValue().toString();
-        String user_name = uname.getText().toString().trim();
-        String user_password = upassword.getText().toString().trim();
-        if (uname.getText().toString().equals(name) && upassword.getText().toString().equals(age)) {
-            Intent intent = new Intent(MainActivity.this, Post.class);
-            startActivity(intent);
-        } else if (uname.length() == 0 || upassword.length() == 0) {
-            Toast.makeText(MainActivity.this, "Fill your name and password", Toast.LENGTH_LONG).show();
-        } else if(name != user_name || age != user_password) {
-            Toast.makeText(MainActivity.this, "User name of password wrong", Toast.LENGTH_LONG).show();
+
+        ConnectivityManager cManager=(ConnectivityManager)getSystemService(this.CONNECTIVITY_SERVICE);
+        NetworkInfo nInfo=cManager.getActiveNetworkInfo();
+        if(nInfo!=null && nInfo.isConnected()){
+            btn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    reff.addValueEventListener(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                            String name = dataSnapshot.child("name").getValue().toString();
+                            String age = dataSnapshot.child("age").getValue().toString();
+                            String user_name = uname.getText().toString().trim();
+                            String user_password = upassword.getText().toString().trim();
+                            if (uname.getText().toString().equals(name) && upassword.getText().toString().equals(age)) {
+                                Intent intent = new Intent(MainActivity.this, Post.class);
+                                startActivity(intent);
+                            } else if (uname.length() == 0 || upassword.length() == 0) {
+                                Toast.makeText(MainActivity.this, "Fill your name and password", Toast.LENGTH_LONG).show();
+                            } else if(name != user_name || age != user_password) {
+                                Toast.makeText(MainActivity.this, "User name of password wrong", Toast.LENGTH_LONG).show();
+                            }
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                        }
+                    });
+                }
+            });
         }
-    }
-
-    @Override
-    public void onCancelled(@NonNull DatabaseError databaseError) {
-
-    }
-});
-            }
-        });
+        else{
+            Toast.makeText(MainActivity.this,"Open Internet",Toast.LENGTH_LONG).show();
+        }
     }
 }
