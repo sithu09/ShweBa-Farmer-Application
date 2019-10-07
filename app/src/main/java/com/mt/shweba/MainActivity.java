adfb33ed6974ac4ce1f09cef9c960508cc75c2ea
Sithu Lwin <lwinsithu3@gmail.com>
2019-10-07 22:23:54 +0630

    2/3 Finishee Login-Signup

--- app/src/main/java/com/mt/shweba/MainActivity.java
+++ app/src/main/java/com/mt/shweba/MainActivity.java
@@ -22,41 +22,43 @@ import com.google.firebase.database.ValueEventListener;
 public class MainActivity extends AppCompatActivity {
 Button l_btn,s_btn;
 EditText uname,upassword;
-DatabaseReference reff;
+DatabaseReference reff,up_reff;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-      l_btn=(Button)findViewById(R.id.login);
-      s_btn=(Button)findViewById(R.id.signup);
-      uname=(EditText) findViewById(R.id.editText);
-      upassword=(EditText)findViewById(R.id.editText2);
-        reff= FirebaseDatabase.getInstance().getReference().child("Member");
+        l_btn = (Button) findViewById(R.id.login);
+        s_btn = (Button) findViewById(R.id.signup);
+        uname = (EditText) findViewById(R.id.editText);
+        upassword = (EditText) findViewById(R.id.editText2);
 
 
-        ConnectivityManager cManager=(ConnectivityManager)getSystemService(this.CONNECTIVITY_SERVICE);
-        NetworkInfo nInfo=cManager.getActiveNetworkInfo();
+        ConnectivityManager cManager = (ConnectivityManager) getSystemService(this.CONNECTIVITY_SERVICE);
+        NetworkInfo nInfo = cManager.getActiveNetworkInfo();
 
-        if(nInfo!=null && nInfo.isConnected()){
+        if (nInfo != null && nInfo.isConnected()) {
             l_btn.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
+                    final String user_name = uname.getText().toString().trim();
+                    final String user_password = upassword.getText().toString().trim();
+                    reff = FirebaseDatabase.getInstance().getReference().child(user_name);
                     reff.addValueEventListener(new ValueEventListener() {
                         @Override
                         public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                            String user_name = uname.getText().toString().trim();
-                            String user_password = upassword.getText().toString().trim();
-                            String name = dataSnapshot.child("name").getValue().toString();
-                            String age = dataSnapshot.child("age").getValue().toString();
-
-                            if (uname.getText().toString().equals(name) && upassword.getText().toString().equals(age)) {
+                            String d_password= dataSnapshot.child("password").getValue().toString();
+                            String d_name= dataSnapshot.child("name").getValue().toString();
+                            if (uname.getText().toString().equals(d_name) && upassword.getText().toString().equals(d_password))
+                            {
                                 Intent intent = new Intent(MainActivity.this, Post.class);
                                 startActivity(intent);
-                            } else if (uname.length() == 0 || upassword.length() == 0) {
+                            }
+                            else if (user_name.length() == 0 || user_password.length() == 0) {
                                 Toast.makeText(MainActivity.this, "Fill your name and password", Toast.LENGTH_LONG).show();
-                            } else if(name != user_name || age != user_password) {
+                            } else if(user_name != d_name || user_password!= d_password) {
                                 Toast.makeText(MainActivity.this, "User name of password wrong", Toast.LENGTH_LONG).show();
                             }
+
                         }
 
                         @Override
@@ -64,24 +66,25 @@ DatabaseReference reff;
 
                         }
                     });
+
                 }
             });
-s_btn.setOnClickListener(new View.OnClickListener() {
-    @Override
-    public void onClick(View v) {
-        String i_name= uname.getText().toString().trim();
-        String i_password = upassword.getText().toString().trim();
-Member member=new Member();
-        member.setName(i_name);
-member.setPassword(i_password);
-                reff.child(i_name).setValue(member);
+            s_btn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    up_reff= FirebaseDatabase.getInstance().getReference();
+                    String i_name = uname.getText().toString().trim();
+                    String i_password = upassword.getText().toString().trim();
+                    Member member = new Member();
+                    member.setName(i_name);
+                    member.setPassword(i_password);
+                    up_reff.child(i_name).setValue(member);
 
-        Toast.makeText(MainActivity.this,"PP",Toast.LENGTH_LONG).show();
-    }
-});
-        }
-        else{
-            Toast.makeText(MainActivity.this,"Open Internet",Toast.LENGTH_LONG).show();
+                    Toast.makeText(MainActivity.this, "PP", Toast.LENGTH_LONG).show();
+                }
+            });
+        } else {
+            Toast.makeText(MainActivity.this, "Open Internet", Toast.LENGTH_LONG).show();
         }
     }
 }
